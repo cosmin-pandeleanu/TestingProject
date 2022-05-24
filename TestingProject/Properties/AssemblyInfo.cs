999ec156f723a6e849b9a87c43b80961d3a7073c
Pandeleanu Cosmin <cosmin.pandeleanu@yahoo.com>
2022-05-24 19:43:32 +0300

    Add project files.

---
+++ TestingProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53bb15ef-fcc1-418b-b56c-517490adfccb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestingProject
+{
+    [TestClass]
+    public class LoginTests
+    {
+        private IWebDriver driver;
+
+        [TestMethod]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver(); // open chrome browser
+            driver.Manage().Window.Maximize(); // maximize the window
+            driver.Navigate().GoToUrl("OUR URL");
+        }
+    }
+}
